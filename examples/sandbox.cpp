02d4eeeacc895df6327d1aed95949dce145b9354
Mohd Zaid <mdzaid0205@gmail.com>
2025-05-09 14:20:04 +0530

    Refactor sandbox.cpp to use Vec2f objects
    Updated `sandbox.cpp` to create and manipulate `Core::Vec2f` objects. Removed the previous method of instantiation and initialized `o` and `p` with specific values. Calculated the difference vector `d` and added output statements to print the values of `o`, `p`, and `d` for verification.

--- examples/sandbox.cpp
+++ examples/sandbox.cpp
@@ -3,7 +3,13 @@
 
 int main(int argc, char** argv){
 
-    Core::Vec2f f;
-    f = Core::newVecNT<Core::Float32, 2>();
+    // Just trying to use those core-defined entities in sanbox to ensure linking.
+    Core::Vec2f o = { 6.0f, 5.0f };
+    Core::Vec2f p = { 1.0f, 3.0f };
+    Core::Vec2f d = { o[0] - p[0], o[1] - p[1] };
+    std::cout << "O : (" << o[0] << ", " << o[1] << ")" << std::endl;
+    std::cout << "P : (" << p[0] << ", " << p[1] << ")" << std::endl;
+    std::cout << "d : (" << d[0] << ", " << d[1] << ")" << std::endl;
+
     return 0;
 }
