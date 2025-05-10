3fcb9317c5c7719a3ec7450b97837145fb31de32
Mohd Zaid <mdzaid0205@gmail.com>
2025-05-10 17:27:48 +0530

    Refactor sandbox.cpp for banner and highlight output
    Removed vector calculations and console output. Added
    banner and highlighted string variables using
    Core::__get_banner and Core::__get_highlight functions.
    Implemented Core::__recurse_output to display the
    banners and highlights in the console.

--- examples/sandbox.cpp
+++ examples/sandbox.cpp
@@ -4,12 +4,16 @@
 int main(int argc, char** argv){
 
     // Just trying to use those core-defined entities in sanbox to ensure linking.
-    Core::Vec2f o = { 6.0f, 5.0f };
-    Core::Vec2f p = { 1.0f, 3.0f };
-    Core::Vec2f d = { o[0] - p[0], o[1] - p[1] };
-    std::cout << "O : (" << o[0] << ", " << o[1] << ")" << std::endl;
-    std::cout << "P : (" << p[0] << ", " << p[1] << ")" << std::endl;
-    std::cout << "d : (" << d[0] << ", " << d[1] << ")" << std::endl;
+    Core::String bannerA = Core::__get_banner<Core::Console::HIG>("DBG"_B, 0x66ff6600, 0x66ff6600, 0x66666600);
+    Core::String bannerB = Core::__get_banner<Core::Console::HIG>("DBG"  , 0x66ff6600, 0x66ff6600, 0x66666600);
+    Core::String bannerC = Core::__get_banner<Core::Console::HIG>("DBG"_D, 0x66ff6600, 0x66ff6600, 0x66666600);
+    Core::String highlightedA = Core::__get_highlight<Core::Console::HIG>("This is how to print some debug to console."_B, 0x66ff6600, 0x66666600);
+    Core::String highlightedB = Core::__get_highlight<Core::Console::HIG>("This is how to print some debug to console."  , 0x66ff6600, 0x66666600);
+    Core::String highlightedC = Core::__get_highlight<Core::Console::HIG>("This is how to print some debug to console."_D, 0x66ff6600, 0x66666600);
+
+    Core::__recurse_output(bannerA, ":"_D, highlightedA);
+    Core::__recurse_output(bannerB, ":"_D, highlightedB);
+    Core::__recurse_output(bannerC, ":"_D, highlightedC);
 
     return 0;
 }
