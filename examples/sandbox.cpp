c68f4655e91a90ca517af9f6a7d5b3f1ff0a6471
Mohd Zaid <mdzaid0205@gmail.com>
2025-05-10 17:33:05 +0530

    Adjust console output levels for banners and highlights
    Updated `sandbox.cpp` to modify the console output levels for banners and highlighted strings.
    - `bannerA` and `bannerB` now use `Core::Console::LOW` and `Core::Console::MED`, respectively, while `bannerC` remains at `Core::Console::HIG`.
    - The highlight for `highlightedA` has been changed to `Core::Console::LOW` with a new suffix (`_U`), while `highlightedB` and `highlightedC` use `Core::Console::MED` and `Core::Console::HIG` with the suffix (`_B`).
    These changes enhance visual clarity and categorization in console output.

--- examples/sandbox.cpp
+++ examples/sandbox.cpp
@@ -4,12 +4,12 @@
 int main(int argc, char** argv){
 
     // Just trying to use those core-defined entities in sanbox to ensure linking.
-    Core::String bannerA = Core::__get_banner<Core::Console::HIG>("DBG"_B, 0x66ff6600, 0x66ff6600, 0x66666600);
-    Core::String bannerB = Core::__get_banner<Core::Console::HIG>("DBG"  , 0x66ff6600, 0x66ff6600, 0x66666600);
-    Core::String bannerC = Core::__get_banner<Core::Console::HIG>("DBG"_D, 0x66ff6600, 0x66ff6600, 0x66666600);
-    Core::String highlightedA = Core::__get_highlight<Core::Console::HIG>("This is how to print some debug to console."_B, 0x66ff6600, 0x66666600);
-    Core::String highlightedB = Core::__get_highlight<Core::Console::HIG>("This is how to print some debug to console."  , 0x66ff6600, 0x66666600);
-    Core::String highlightedC = Core::__get_highlight<Core::Console::HIG>("This is how to print some debug to console."_D, 0x66ff6600, 0x66666600);
+    Core::String bannerA = Core::__get_banner<Core::Console::LOW>("DBG", 0x66ff6600, 0x66ff6600, 0x66666600);
+    Core::String bannerB = Core::__get_banner<Core::Console::MED>("DBG", 0x66ff6600, 0x66ff6600, 0x66666600);
+    Core::String bannerC = Core::__get_banner<Core::Console::HIG>("DBG", 0x66ff6600, 0x66ff6600, 0x66666600);
+    Core::String highlightedA = Core::__get_highlight<Core::Console::LOW>("This is how to print some debug to console."_U, 0x66ff6600, 0x66666600);
+    Core::String highlightedB = Core::__get_highlight<Core::Console::MED>("This is how to print some debug to console."_B, 0x66ff6600, 0x66666600);
+    Core::String highlightedC = Core::__get_highlight<Core::Console::HIG>("This is how to print some debug to console."_B, 0x66ff6600, 0x66666600);
 
     Core::__recurse_output(bannerA, ":"_D, highlightedA);
     Core::__recurse_output(bannerB, ":"_D, highlightedB);
