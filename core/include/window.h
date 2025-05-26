f503ac464e25b820c56f318074549d91da3491e2
Mohd Zaid <mdzaid0205@gmail.com>
2025-05-26 20:40:53 +0530

    Add IWindowBase class for window management
    Introduce the `IWindowBase` class in the `Core::Window::Base` namespace to handle window management. This class includes properties for window dimensions, name, and state, with a constructor for initialization and a destructor for cleanup. Also, include necessary headers `platform.h` and `pch.h`.

--- core/include/window.h
+++ core/include/window.h
@@ -1,4 +1,29 @@
 #ifndef CORE_WINDOW_H
 #define CORE_WINDOW_H
 
+#include "platform.h"
+#include "pch.h"
+
+namespace Core::Window {
+	namespace Base {
+		class IWindowBase {
+		private:
+			GLFWwindow* instance;
+		protected:
+			uInt32 __window_w;
+			uInt32 __window_h;
+			String __window_name;
+			Byte _is_window_open;
+		public:
+			IWindowBase(String name, uInt32 w, uInt32 h);
+			~IWindowBase();
+		public:
+			void updateWinInstances();
+			void makeContextCurrent();
+			sInt32 isWindowDestroyed();
+		};
+	};
+
+};
+
 #endif
