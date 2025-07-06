d2acaf118ff19710474eae8dbf9e08453da8f824
Mohd Zaid <mdzaid0205@gmail.com>
2025-07-06 13:46:41 +0530

    Enhance linked list implementation and update .gitignore
    - Updated `.gitignore` to ignore build and temp files.
    - Introduced `uLinkedList` and `bLinkedList` classes in `nodes.h`.
    - Enhanced `uNode` and `bNode` with detailed comments and correct method returns.
    - Implemented methods for node management in `uLinkedList` and `bLinkedList`.
    - Improved memory management and exception handling in `nodes.inl`.
    - Updated `insert` and `remove` methods for robust index handling.
    - Overall improvements in functionality, readability, and memory management.

--- structures/include/nodes.h
+++ structures/include/nodes.h
@@ -6,14 +6,39 @@
 
 namespace Structures {
 
-    template<typename Type> class uNode;        // uni-directional
-    template<typename Type> class bNode;        // bi-directional
+    template<typename T> class uNode;               // uni-directional node
+    template<typename T> class bNode;               // bi-directional node
+    template<typename T> class uLinkedList;         // uni-directional linked list
+    template<typename T> class bLinkedList;         // bi-directional linked list
 
-    template<typename T> using uN = uNode<T>;   // shorthand for uNode
-    template<typename T> using bN = bNode<T>;   // shorthand for bNode
+#pragma region typeAcronyms
 
+    template<typename T> using uN = uNode<T>;
+    template<typename T> using bN = bNode<T>;
+    template<typename T> using uLL = uLinkedList<T>;
+    template<typename T> using bLL = bLinkedList<T>;
 
-    // uni-directional Node Implementation
+#pragma endregion
+
+#pragma region uNodeImplementation
+    /*
+    * @brief *uni-directional* node (contains refernce to only the next node)
+    *
+    * @details
+    * conventional one-directional node that keeps information about its next node in sequence.
+    * usage consist typically to make linked list (unidirectional) and impliment stack and queue.
+    *
+    * @tparam T  Type of the implementation class.
+    *
+    * @par Usage
+    * @code
+    * uNode<int>* node = new uNode<int>(value1);        // declare a node
+    * uNode<int>* next = new uNode<int>(value2);        // declare next node
+    * node->setNext(next);                              // make a link between nodes
+    * @endcode
+    *
+    *  connect it in a manner as : [NODE] -> [NEXT] -> |nullptr|
+    */
     template<typename Type> class uNode {
     private:
         Type* __item;                           // store pointer to the element.
@@ -32,8 +57,27 @@ namespace Structures {
         void setItem(Type* item) { __item = item; }
         void setNext(uNode<Type>* next) { __next = next; }
     };
+#pragma endregion
 
-    // bi-directional Node Implementation
+#pragma region nNodeImplementaion
+    /*
+    * @brief *bi-directional* node (contains refernce to next & previous nodes)
+    *
+    * @details
+    * conventional bi-directional node that keeps information about its next and previous nodes.
+    * usage consist typically to make linked list (bi-directional) and impliment dequeue.
+    *
+    * @tparam T  Type of the implementation class.
+    *
+    * @par Usage
+    * @code
+    * bNode<int>* prev = new bNode<int>(value0);        // declare prev node
+    * bNode<int>* node = new bNode<int>(value1);        // declare a node
+    * bNode<int>* next = new bNode<int>(value2);        // declare next node
+    * @endcode
+    *
+    * connect it in a manner as : |nullptr| <- [PREV] <=> [NODE] <=> [NEXT] -> |nullptr|
+    */
     template<typename Type> class bNode {
     private:
         Type* __item;                           // store pointer to the element.
@@ -53,27 +97,37 @@ namespace Structures {
         }
     public:
         Type* getItem() { return __item; }
-        uNode<Type>* getNext() { return __next; }
-        uNode<Type>* getPrev() { return __prev; }
+        bNode<Type>* getNext() { return __next; }
+        bNode<Type>* getPrev() { return __prev; }
         void setItem(Type* item) { __item = item; }
-        void setNext(uNode<Type>* next) { __next = next; }
-        void setPrev(uNode<Type>* prev) { __prev = prev; }
+        void setNext(bNode<Type>* next) { __next = next; }
+        void setPrev(bNode<Type>* prev) { __prev = prev; }
     };
+#pragma endregion
 
+#pragma region un-neccessesary-insight
     // One may use this for unideirectional or bidirectional Liked List as follows, i dont feel the need to make a linked
     // list class for this purpose, i will however make required functions to support operations on linked lists.
     // (change of plans) i am implimenting Linked list class for making it more efficient to use Linked List.
     // linked list for our case is just a pointer to a nNode<Type>, havent planned for this yet.
-    
-    template<typename T> class uLinkedList;
-    template<typename T> class bLinkedList;
+#pragma endregion
 
-    // unidorectional Liked list implimentaion
+#pragma region uLinkedListImplementaion
+    /**
+    * @brief *uni-directional* linked list (built using uNode)
+    *
+    * @details
+    * conventional one-directional linked list that stores a sequence of nodes connected in a forward-only manner.
+    * provides basic operations like insert, remove, search, and traversal.
+    * internally uses uNode to manage each element and the pointer to the next node.
+    *
+    * @tparam T  Type of the data stored in the list.
+    */
     template<typename T> class uLinkedList {
     private:
         uN<T>* __head;          // Store head of linked list
         uN<T>* __tail;          // Store tail (not madatory but is there for perfoemance in some cases).
-        int __size;             // Specifically for converson to other promitive types.
+        int __size;             // Specifically for conversion to other primitive types.
 
     public:
         uLinkedList() : __head(nullptr), __tail(nullptr), __size(0) {}
@@ -84,19 +138,62 @@ namespace Structures {
         // stack -> attachL and detachL. LIFO
         // queue -> attachR and detachL. FIFO
 
-        void attachL(T item);       // to attach a node to left of the linked list. { visually: |new| -> |head| -> |...| -> |tail| }
-        void attachR(T item);       // to attach a node to right of the linked list. { visually: |head| -> |...| -> |tail| -> |new| }
-        T& detachL();        // to detach left most and move head to next. { returns: head, moves to next element to head and detaches previous. }
-        T& detachR();        // to detach right most node. { returns: tail, no need to previous of tail becomes new tail. }
+        void attachL(T item);                   // to attach a node to left of the linked list.
+        void attachR(T item);                   // to attach a node to right of the linked list.
+        T& detachL();                           // to detach left most and move head to next.
+        T& detachR();                           // to detach right most node.
 
         void insert(T item, int index);         // insert an item to specified index.
         T& remove(int index);                   // remove element form specified index.
         uN<T>& get(int index);                  // get element at specified index.
     };
+#pragma endregion
 
+#pragma region bLinkedListImplementation
+    /**
+    * @brief *bi-directional* linked list (built using bNode)
+    *
+    * @details
+    * conventional two-way linked list where each node stores a reference to both its previous and next nodes.
+    * allows traversal in both directions and supports more efficient insert/remove operations from either end or middle.
+    * internally uses bNode to manage each element and its neighboring connections.
+    *
+    * @tparam T  Type of the data stored in the list.
+    */
+    template<typename T> class bLinkedList {
+    private:
+        bN<T>* __head;          // Store head of linked list
+        bN<T>* __tail;          // Store tail (same reason as previous).
+        int __size;             // Specifically for conversion to other primitive types.
+
+    public:
+        bLinkedList() : __head(nullptr), __tail(nullptr), __size(0) {}
+
+    public:
+
+        // attach<DIRECTION> and detach<DIRECTION> is there only for special purposes like to make stack and queue.
+        // stack -> attachL and detachL. LIFO, unidirectional
+        // queue -> attachR and detachL. FIFO, bidirectional
+        // it makes sense that bidirectional LL is used for this purpose.
+
+        void attachL(T item);                   // to attach a node to left of the linked list.
+        void attachR(T item);                   // to attach a node to right of the linked list. 
+        T& detachL();                           // to detach left most and move head to next.
+        T& detachR();                           // to detach right most node.
+
+        void insert(T item, int index);         // insert an item at specified index.
+        T& remove(int index);                   // remove element from specified index.
+        bN<T>& get(int index);                  // get element at specified index.
+    };
+
+#pragma endregion
+
+#pragma region typeConversionFunctions
     // TODO: impliment type conversions from Linked List to an array and from an array to Linked List
     // TODO: impliment variant for both dynamic and atatic array <--> uni and bi directional LL.
-}
+#pragma endregion
+
+};
 
 #include "nodes.inl"
 
--- structures/include/nodes.inl
+++ structures/include/nodes.inl
@@ -1,114 +1,269 @@
 #include "nodes.h"
 
-template<typename T>
-inline void Structures::uLinkedList<T>::attachR(T item){
-	if (__tail != nullptr)
-		__tail->setNext(new Structures::uNode<T>(item));
-	else {
-		__tail = new Structures::uNode<T>(item);
-		__head = __tail;
-	}
-	__size++;
-}
+#define TEMPLATED_T template<typename T>
+#define ENF_EXCEPTION Exceptions::ElementNotFound("Cannot remove element from empty LL.")
+#define IOR_EXCEPTION Exceptions::IndexOutOfRange("Cannot find specifed `_index` in LL.")
+#define INCREMENT __size++
+#define DECREMENT __size--
+#define ZERO_ 0
+#define LAST_ __size - 1
+#define AND ;
+#define EXIT return
 
-template<typename T>
-inline void Structures::uLinkedList<T>::attachL(T item) {
-	__head = new Structures::uNode<T>(item, __head);
+#pragma region uLinkedListMethods
+
+TEMPLATED_T
+inline void Structures::uLinkedList<T>::attachR(T item)
+{
 	if (__tail == nullptr)
-		__tail = __head;
-	__size++;
+		__tail = __head = new Structures::uNode<T>(item);
+	else {
+		__tail->setNext(new Structures::uNode<T>(item));
+		__tail = __tail->getNext();
+	}
+	INCREMENT;
 }
 
-template<typename T>
-inline T& Structures::uLinkedList<T>::detachL(){
-	if (__size == 0)
-		throw Exceptions::ElementNotFound("Cannot detachLeft from an empty u-linked list")
+TEMPLATED_T
+inline void Structures::uLinkedList<T>::attachL(T item)
+{
+	__head = new Structures::uNode<T>(item, __head);
+	if (__size == ZERO_) __tail = __head;
+	INCREMENT;
+}
 
+TEMPLATED_T
+inline T& Structures::uLinkedList<T>::detachR()
+{
+	if (__size == 0) throw ENF_EXCEPTION;
+	T* __returnValue = __tail->getItem();
+
+	uN<T>* __parent = __head;
+	while (__parent->getNext() != __tail) {
+		__parent = __parent->getNext();
+	}
+
+	__tail->setItem(nullptr); delete __tail;
+	__tail = __parent;
+	DECREMENT;
+	return __returnValue;
+}
+
+TEMPLATED_T
+inline T& Structures::uLinkedList<T>::detachL()
+{
+	if (__size == 0) throw ENF_EXCEPTION;
 	uNode<T>* __next = __head->getNext();
-	T* __returnableT = __head->getItem();
-	
+	T* __returnValue = __head->getItem();
+
 	__head->setNext(nullptr);
 	__head->setItem(nullptr);
-	
 	delete __head;
 
-	if (__next == nullptr)
-		__tail = nullptr;
+	if (__next == nullptr) __tail = nullptr;
 
 	__head = __next;
-	__size--;
-	return __returnableT;
+	DECREMENT;
+
+	return __returnValue;
 }
 
-template<typename T>
-inline T& Structures::uLinkedList<T>::detachR(){
-	if (__size == 0)
-		throw Exceptions::ElementNotFound("Cannot detachRight from an empty u-linked list")
+TEMPLATED_T
+inline void Structures::uLinkedList<T>::insert(T item, int index)
+{
+	if (__size > index) throw IOR_EXCEPTION;
+	if (index == ZERO_) attachL(item) AND EXIT;
+	if (index == LAST_) attachR(item) AND EXIT;
 
-	T* __returnableT = __tail->getItem();
-	__tail->setItem(nullptr);
-	delete __tail;
-
-	uN<T>* __currentN = __head;
-	while (__currentN->getNext() != nullptr) {
-		__currentN = __currentN->getNext();
-	}
-	__tail = __currentN;
-	__size--;
-	return __returnableT;
-}
-
-template<typename T>
-inline void Structures::uLinkedList<T>::insert(T item, int index){
-	if (__size < index)
-		throw Exceptions::IndexOutOfRange("Cannot insert at index > size in a linked list");
-	
-	if (index == 0) {
-		this->attachL(item);
-		return;
-	}
-
-	uN<T>* __currentN = __head;
-	for (int i = 0; i < index; i++){
-		__currentN = __currentN->getNext();
-	}
-
-	uN<T>* __insert = new uN<T>(item, __currentN->getNext());
-	__currentN->setNext(__insert);
-}
-
-template<typename T>
-inline T& Structures::uLinkedList<T>::remove(int index){
-	if (__size <= index)
-		throw Exceptions::IndexOutOfRange("Cannot remove from index >= size in a linked list");
-
-	uN<T>* __currentN = __head;
-	for (int i = 0; i < index-1; i++) {
-		__currentN = __currentN->getNext();
-	}
-
-	uN<T>* __element = __currentN->getNext();
-	__currentN->setNext(__element->getNext());
-	T* __returnableT = __element->getItem();
-	__element->setNext(nullptr);
-	__element->setItem(nullptr);
-
-	delete __element;
-	return __returnableT;
-}
-
-template<typename T>
-inline uN<T>& Structures::uLinkedList<T>::get(int index){
-	if (__size <= index)
-		throw Exceptions::IndexOutOfRange("Cannot get element at index >= size in a linked list");
-
-	uN<T>* __currentN = __head;
+	uN<T>* __parent = __head;
 	for (int i = 0; i < index; i++) {
-		__currentN = __currentN->getNext();
+		__parent = __parent->getNext();
 	}
 
-	return __currentN;
+	__parent->setNext(new uN<T>(item, __parent->getNext()));
+	INCREMENT;
 }
 
+TEMPLATED_T
+inline T& Structures::uLinkedList<T>::remove(int index)
+{
+	if (index >= __size) throw IOR_EXCEPTION;
+	if (index == ZERO_) return detachL();
+	if (index == LAST_) return detachR();
 
+	uN<T>* __parent = __head;
+	for (int i = 0; i < index - 1; i++) {
+		__parent = __parent->getNext();
+	}
 
+	uN<T>* __removable = __parent->getNext();
+	__parent->setNext(__removable->getNext());
+	T* __returnValue = __removable->getItem();
+	__removable->setNext(nullptr);
+	__removable->setItem(nullptr);
+
+	delete __removable;
+	DECREMENT;
+
+	return __returnValue;
+}
+
+TEMPLATED_T
+inline uN<T>& Structures::uLinkedList<T>::get(int index)
+{
+	if (__size > index) throw IOR_EXCEPTION;
+
+	uN<T>* __returnable = __head;
+	for (int i = 0; i < index - 1; i++) {
+		__returnable = __returnable->getNext();
+	}
+
+	return __returnable;
+}
+
+#pragma endregion
+
+#pragma region bLinkedListMethods
+
+TEMPLATED_T
+inline void Structures::bLinkedList<T>::attachR(T item)
+{
+	if (__tail == nullptr)
+		__tail = __head = new Structures::bNode<T>(item);
+	else {
+		__tail->setNext(new Structures::bNode<T>(item, nullptr, __tail));
+		__tail = __tail->getNext();
+	}
+	INCREMENT;
+}
+
+TEMPLATED_T
+inline void Structures::bLinkedList<T>::attachL(T item)
+{
+	bN<T>* __item = new Structures::bNode<T>(item, __head, nullptr);
+	if (__size == ZERO_) __tail = __head = __item;
+	else __head->setPrev(__item) AND __head = __item;
+	INCREMENT;
+}
+
+TEMPLATED_T
+inline T& Structures::bLinkedList<T>::detachR()
+{
+	if (__size == 0) throw ENF_EXCEPTION;
+	T* __returnValue = __tail->getItem();
+
+	__tail = __tail->getPrev();
+	__tail->getNext()->setItem(nullptr);
+	__tail->getNext()->setPrev(nullptr);
+
+	delete __tail->getNext();
+	__tail->setNext(nullptr);
+	DECREMENT;
+
+	return __returnValue;
+}
+
+TEMPLATED_T
+inline T& Structures::bLinkedList<T>::detachL()
+{
+	if (__size == 0) throw ENF_EXCEPTION;
+	bNode<T>* __next = __head->getNext();
+	T* __returnValue = __head->getItem();
+
+	__head->setNext(nullptr);
+	__head->setItem(nullptr);
+	__next->setPrev(nullptr);
+
+	delete __head;
+	__head = __next;
+	DECREMENT;
+
+	return __returnValue;
+}
+
+TEMPLATED_T
+inline void Structures::bLinkedList<T>::insert(T item, int index)
+{
+	if (__size > index) throw IOR_EXCEPTION;
+	if (index == ZERO_) attachL(item) AND EXIT;
+	if (index == LAST_) attachR(item) AND EXIT;
+
+	bN<T>* __parent;
+	if (index < size / 2) {
+		__parent = __head;
+		for (int i = 0; i < index; i++) {
+			__parent = __parent->getNext();
+		}
+	}
+	else {
+		__parent = __tail;
+		for (int i = __size; i > index; i--) {
+			__parent = __parent->getPrev();
+		}
+	}
+
+	__parent->setNext(new bN<T>(item, __parent->getNext(), __parent));
+	__parent->getNext()->getNext()->setPrev(__parent->getNext());
+
+	INCREMENT;
+}
+
+TEMPLATED_T
+inline T& Structures::bLinkedList<T>::remove(int index)
+{
+	if (index >= __size) throw IOR_EXCEPTION;
+	if (index == ZERO_) return detachL();
+	if (index == LAST_) return detachR();
+
+	bN<T>* __parent;
+	if (index < size / 2) {
+		__parent = __head;
+		for (int i = 0; i < index; i++) {
+			__parent = __parent->getNext();
+		}
+	}
+	else {
+		__parent = __tail;
+		for (int i = __size; i > index; i--) {
+			__parent = __parent->getPrev();
+		}
+	}
+
+	bN<T>* __child = __parent->getNext()->getNext();
+	T* __returnValue = __parent->getNext()->getItem();
+	__parent->getNext()->setNext(nullptr);
+	__parent->getNext()->setPrev(nullptr);
+	__parent->getNext()->setItem(nullptr);
+	delete __parent->getNext();
+	__child->setPrev(__parent);
+	__parent->setNext(__child);
+	DECREMENT;
+
+	return __returnValue;
+}
+
+TEMPLATED_T
+inline bN<T>& Structures::bLinkedList<T>::get(int index)
+{
+	if (index >= __size) throw IOR_EXCEPTION;
+	if (index == ZERO_) return __head;
+	if (index == LAST_) return __tail;
+
+	bN<T>* __parent;
+	if (index < size / 2) {
+		__parent = __head;
+		for (int i = 0; i < index; i++) {
+			__parent = __parent->getNext();
+		}
+	}
+	else {
+		__parent = __tail;
+		for (int i = __size; i > index; i--) {
+			__parent = __parent->getPrev();
+		}
+	}
+
+	return __parent->getNext();
+}
+
+#pragma endregion
