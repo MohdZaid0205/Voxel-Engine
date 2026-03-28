a99acf631034934b00923aa6946dd941190e7704
Mohd Zaid <mdzaid0205@gmail.com>
2026-03-28 14:30:00 +0530

    Remove legacy MainWindow and update App entry point

--- editor/App.xaml.cs
+++ editor/App.xaml.cs
@@ -3,20 +3,20 @@ using System.Configuration;
 using System.Data;
 using System.Windows;
 
-namespace editor
+namespace Editor
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
-        [STAThread]
-        public static void Main()
-        {
-            App app = new();
-            app.InitializeComponent();
-            app.Run();
-        }
+        //[STAThread]
+        //public static void Main()
+        //{
+        //    App app = new();
+        //    app.InitializeComponent();
+        //    app.Run();
+        //}
     }
 
 }
