628af8f0ccd2ae1353b81f4ef754248dd6d6df12
Mohd Zaid <mdzaid0205@gmail.com>
2026-05-31 20:05:40 +0530

    update ViewModelBase

--- editor/Core/ViewModelBase.cs
+++ editor/Core/ViewModelBase.cs
@@ -6,3 +6,11 @@ namespace Editor.Core
 {
     public abstract class ViewModelBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+
+            field = value;
