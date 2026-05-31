4f7d7c93b7e5bd11243e0daa84ef555ca11c3de0
Mohd Zaid <mdzaid0205@gmail.com>
2026-05-31 05:37:04 +0530

    update RelayCommand

--- editor/Core/RelayCommand.cs
+++ editor/Core/RelayCommand.cs
@@ -7,3 +7,12 @@ namespace Editor.Core
     {
         private readonly Action<object?> _execute;
         private readonly Func<object?, bool>? _canExecute;
+
+        public RelayCommand(Action execute, Func<bool>? canExecute = null)
+            : this(_ => execute(), canExecute is null ? null : _ => canExecute())
+        {
+        }
+
+        public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
