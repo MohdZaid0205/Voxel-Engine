f6dbffd101f6ebe8fba22f2f1572e544d7a41b43
Mohd Zaid <mdzaid0205@gmail.com>
2026-05-31 17:32:27 +0530

    add ViewModelBase

---
+++ editor/Core/ViewModelBase.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Editor.Core
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
