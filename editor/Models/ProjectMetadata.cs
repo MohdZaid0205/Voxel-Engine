cfd7ee2af405df9ea27fd664c32e1062c65dfcbb
Mohd Zaid <mdzaid0205@gmail.com>
2026-06-01 11:00:36 +0530

    update ProjectMetadata

--- editor/Models/ProjectMetadata.cs
+++ editor/Models/ProjectMetadata.cs
@@ -5,3 +5,10 @@ using System.Text.Json.Serialization;
 namespace Editor.Models
 {
     public class ProjectMetadata
+    {
+        public string Name { get; set; }
+        public string Base { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string? Author { get; set; }
+
