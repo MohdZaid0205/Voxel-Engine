9bd9080ad57a3981cfa0fc3ee1ff2abf7e7a9789
Mohd Zaid <mdzaid0205@gmail.com>
2026-06-04 12:43:56 +0530

    add NewProjectScreen.xaml

---
+++ editor/Views/Screens/NewProjectScreen.xaml.cs
@@ -0,0 +1,11 @@
+using Editor.ViewModels;
+using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Editor.Views.Screens
+{
+    public partial class NewProjectScreen : Window
+    {
+        public NewProjectScreen()
+        {
