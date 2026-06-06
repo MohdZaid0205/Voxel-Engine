b0809f7e2d03b0ac7d763867b58d70e5b84e774b
Mohd Zaid <mdzaid0205@gmail.com>
2026-06-06 23:09:08 +0530

    improve VoxleEditorView.xaml

--- editor/Views/VoxleEditorView.xaml.cs
+++ editor/Views/VoxleEditorView.xaml.cs
@@ -12,3 +12,10 @@ namespace Editor.Views
 
         private void Body_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
