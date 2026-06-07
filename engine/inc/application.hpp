52cd62aebd0e77f7ee0824d95cc7e1d64e2732b8
Mohd Zaid <mdzaid0205@gmail.com>
2026-06-07 14:25:19 +0530

    update application

--- engine/inc/application.hpp
+++ engine/inc/application.hpp
@@ -31,3 +31,36 @@ namespace Engine {
 
 		// user defined function that runs when event::windowResize is triggered.
 		// it is not necessary to use this callback, in case of unavailablity of
+		// such function, no default action is taken, unless it is specified to 
+		// initialize window as resizeable. or any other default behaviour is specified.
+		using WindowCallbackFunction = void (*)(GLFWwindow*, u32, u32);
+
+		enum WindowCallbackType {
+			RESIZABLE = 0x00,		//> allows window to be resizable by user cations.
+			FIXEDSIZE = 0x01,		//> user actions cannot alter size of window.
+			NODEFAULT = 0x10,		//> resize the window without any default callback.
+		};							//> REDUNDANT (as of now)
+
+		WindowCallbackFunction application_window_callback;
+
+	protected:
+		Application(GLFWwindow* window, String title, u32 w, u32 h,
+			WindowCallbackFunction window_on_resize_callback_function);
+	public:
+		virtual ~Application() = 0;
+
+		// I'am aware that using instance(...) for both creation and retreival is bad but
+		// here i am doing this and no one can stop me;
+
+		static Application& instance();
+
+		// [DEPRICATED]
+		// Application& instance(GLFWwindow* window, String title, u32 w, u32 h);
+		// Application& instance(GLFWwindow* window, String title, u32 w, u32 h,
+		//	  WindowCallbackFunction window_on_resize_callback_function);
+
+	public:
+
+		// all valid getters for private attributes belonging to Application
+
+		LayerStack& get_application_layer_stack();
