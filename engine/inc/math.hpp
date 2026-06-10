4051cddb4b319d7f07e21e5dfda6e6b3f1e06b3c
Mohd Zaid <mdzaid0205@gmail.com>
2026-06-10 12:54:03 +0530

    refactor math

--- engine/inc/math.hpp
+++ engine/inc/math.hpp
@@ -40,3 +40,15 @@ namespace Engine {
         Vector3& operator/=(T scalar) { x /= scalar; y /= scalar; z /= scalar; return *this; }
 
         // Equality Operators
+        bool operator==(const Vector3& v) const { return x == v.x && y == v.y && z == v.z; }
+        bool operator!=(const Vector3& v) const { return !(*this == v); }
+    };
+
+    // FIXME: move this to api.hpp (may break things)
+    // .. (vector aliases) -----------------------------------------------------------+
+    using Vec2df = Vector2<f32>;                                  // 2D Float Vector  |
+    using Vec3df = Vector3<f32>;                                  // 3D Float Vector  |
+    using Vec2di = Vector2<i32>;                                  // 2D Int Vector    |
+    using Vec3di = Vector3<i32>;                                  // 3D Int Vector    |
+    // -------------------------------------------------------------------------------+
+}
