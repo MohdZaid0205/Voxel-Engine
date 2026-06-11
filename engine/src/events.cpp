54c80e8ee06d34ef4b4f4d7cffd4422ea0ec9db8
Mohd Zaid <mdzaid0205@gmail.com>
2026-06-12 00:14:22 +0530

    update events

--- engine/src/events.cpp
+++ engine/src/events.cpp
@@ -7,3 +7,12 @@ ENGINE_API Engine::WindowClosedEvent* Engine::CreateWindowClosedEvent(){
 ENGINE_API Engine::WindowResizedEvent* Engine::CreateWindowResizedEvent(u32 w, u32 h){
 	return new WindowResizedEvent(w, h);
 }
+
+ENGINE_API Engine::KeyPressedEvent* Engine::CreateKeyPressedEvent(i32 k, i32 r){
+	return new KeyPressedEvent(k, r);
+}
+
+ENGINE_API Engine::KeyReleasedEvent* Engine::CreateKeyReleasedEvent(i32 k){
+	return new KeyReleasedEvent(k);
+}
+
